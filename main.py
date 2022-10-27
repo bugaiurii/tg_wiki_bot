5270a841c8f832bc9e8bf0cad856b50e311562aa
Buga Iurii <bugaiurii@yandex.ru>
2022-10-27 20:28:13 +0300

    complete

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+import wikipedia
+import re
+
+
+bot = telebot.TeleBot('5742441906:AAFBEUGLuzsNQoiTvjBJHQYvId71WW0_A58')
+
+wikipedia.set_lang("ru")
+
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        wikitext = ny.content[:1000]
+        wikimas = wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not('==' in x):
+                if (len((x.strip()))>3):
+                    wikitext2 = wikitext2 + x + '.'
+            else:
+                break
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
+
+        return wikitext2
+
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+
+
+bot.polling(none_stop=True, interval=0)
